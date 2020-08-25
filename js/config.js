28f6c58605897fbc373ab1552eb473a8fd824abf
DESKTOP-7UMMBFL\2014s <john.von.ludendorff@gmail.com>
2020-08-25 17:55:11 -0400

    8

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "DNG Vietnam RP";
+Config.title = "[DNG] 7th Panzer Division RP";
 
 /**
  * Enable map text in the top left corner of the screen?
